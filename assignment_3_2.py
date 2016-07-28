c6ea3239a27b46dea039683f69ce166e9457eec0
cm080691 <more08.chetan@gmail.com>
2016-07-28 22:58:40 +0000

    assignment_3

--- assignment_3_2.py
+++ assignment_3_2.py
@@ -2,11 +2,18 @@ import json
 
 class MySerializer(object):
 
+    jsonstr = {'y': 2, 'x': 1, 'z': 3}
+
     @classmethod
     def deserializer(cls, jsonstr):
         #load from json to dict
         #initialize object, return
         json_data = json.loads(jsonstr)
 
-        return cls(**d)
+        return cls(**json_data)
 
+    def serializer(self):
+        # iterate over self.my_attrs
+        #get attrs, set into dictionary
+        #return dumps(dictionary)
+        pass
